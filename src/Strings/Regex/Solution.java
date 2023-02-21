0d4be893a3b94712ed071ecf19aa67f73472812f
Martin Parunev <parunev@gmail.com>
2023-02-21 13:59:49 +0200

    commit

---
+++ src/Strings/Regex/Solution.java
@@ -0,0 +1,20 @@
+package Strings.Regex;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNextLine()){
+            String ip = scanner.nextLine();
+            System.out.println(ip.matches(new MyRegex().pattern));
+        }
+
+    }
+
+    static class MyRegex{
+        String num = "([01]?\\d{1,2}|2[0-4]\\d|25[0-5])";
+        String pattern = num + "." + num + "." + num + "." + num;
+    }
+}
